c164abd3463c864b8c95c7fdd2a509b7c915eed3
mohamedabdelstar06 <mohamedabdelstar06@gmail.com>
2025-10-16 16:08:59 +0300

    Add login and logout feature

--- Backend/SkyLearnApi/Enums/UserRole.cs
+++ Backend/SkyLearnApi/Enums/UserRole.cs
@@ -7,3 +7,4 @@ namespace SkyLearnApi.Enums
         Admin
     }
 }
+ 
--- Backend/SkyLearnApi/Program.cs
+++ Backend/SkyLearnApi/Program.cs
@@ -1,15 +1,41 @@
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 using SkyLearnApi.Data;
+using SkyLearnApi.Services.Implementations;
+using SkyLearnApi.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+// Serilog
+builder.Host.UseSerilog((context, config) =>
+    config.ReadFrom.Configuration(context.Configuration));
 
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<JwtService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add JWT Auth
+var jwtKey = builder.Configuration["Jwt:Key"];
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+        };
+    });
 
 var app = builder.Build();
 
