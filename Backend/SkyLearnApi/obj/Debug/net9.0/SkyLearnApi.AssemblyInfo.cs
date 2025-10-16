055971ba12c5b4886c53c2f122259159c75fc242
mohamedabdelstar06 <mohamedabdelstar06@gmail.com>
2025-10-16 12:37:33 +0300

    Save current changes before switching branch

--- Backend/SkyLearnApi/obj/Debug/net9.0/SkyLearnApi.AssemblyInfo.cs
+++ Backend/SkyLearnApi/obj/Debug/net9.0/SkyLearnApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SkyLearnApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc4ab2baacac69006dd51fb03b5f08241381db00")]
 [assembly: System.Reflection.AssemblyProductAttribute("SkyLearnApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SkyLearnApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
